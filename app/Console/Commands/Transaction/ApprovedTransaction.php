43a5bcb6f20d7ada3588f5c3bd5f5dab9b89d642
Bruno Costa <bruno.costa@superlogica.com>
2022-04-11 11:48:12 -0300

    nao finalizado

--- app/Console/Commands/Transaction/ApprovedTransaction.php
+++ app/Console/Commands/Transaction/ApprovedTransaction.php
@@ -6,6 +6,7 @@ use App\Services\TransactionService;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ApprovedTransaction extends Command
 {
@@ -37,6 +38,8 @@ class ApprovedTransaction extends Command
 
             if (!empty($rs)) {
                 $transactionService->transactionApprroved($rs);
+            } else {
+                Log::error('transaction ' . $data['uuid'] . ' do not exist');
             }
 
         });
