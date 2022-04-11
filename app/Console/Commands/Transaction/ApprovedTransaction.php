7aad79414243815f54b8a4af53f9ea6d8bc9b6b7
Bruno Costa <bruno.costa@superlogica.com>
2022-04-11 11:34:11 -0300

    finalizado

--- app/Console/Commands/Transaction/ApprovedTransaction.php
+++ app/Console/Commands/Transaction/ApprovedTransaction.php
@@ -30,7 +30,7 @@ class ApprovedTransaction extends Command
      */
     public function handle(TransactionService $transactionService)
     {
-        app('pubsub')->consume('queue_transaction_approved' . config('codepix.credential'), [
+        app('pubsub')->consume('queue_transaction_approved.' . config('codepix.credential'), [
             'transaction.approved.' . config('codepix.credential')
         ], function ($data) use ($transactionService) {
             $rs = $transactionService->find($data['uuid']);
